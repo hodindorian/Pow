faf04fe0391b7caaa6937552e23e0948193fe38f
aurian.jault <aurian.jault@etu.uca.fr>
2024-06-05 09:20:27 +0200

    print number of  column and rows + missing values quantity

--- src/load_csv.py
+++ src/load_csv.py
@@ -1,8 +1,21 @@
-#!/usr/bin/env python3
 import pandas as pd
 
 
 def csv_value():
     df = pd.read_csv('./data.csv')
-    print(df.head())
+    # print(df.head())
+
+    #print all detail 
+    # df.info()
+
+    # Print number of missing value for each column
+    # print(df.isna().sum())
+
+    # Useless values
+    # Off-Road Description         -> 156170
+    # Municipality                 -> 152979
+    # Related Non-Motorist         -> 166642
+    # Non-Motorist Substance Abuse -> 167788
+    # Circumstance                 -> 140746
+
     return df
